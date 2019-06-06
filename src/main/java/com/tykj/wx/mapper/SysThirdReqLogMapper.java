b06634b7439dfb51ee365cd8b8a180abfa53f55f
BUCHU <710625937@qq.com>
2019-06-06 18:58:27 +0800

    发送消息到微信

---
+++ src/main/java/com/tykj/wx/mapper/SysThirdReqLogMapper.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.mapper;
+
+import com.tykj.wx.entity.SysThirdReqLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* <p>
+    *  Mapper 接口
+    * </p>
+*
+* @author huran
+* @since 2019-06-06
+*/
+public interface SysThirdReqLogMapper extends BaseMapper<SysThirdReqLog> {
+
+}
