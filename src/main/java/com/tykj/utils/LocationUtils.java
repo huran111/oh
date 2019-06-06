5e3845cdd1763088ff300f7a53488ae2b6acec3c
BUCHU <710625937@qq.com>
2019-06-06 10:03:08 +0800

    提交文件程序111111

--- src/main/java/com/tykj/utils/LocationUtils.java
+++ src/main/java/com/tykj/utils/LocationUtils.java
@@ -1,6 +1,7 @@
 package com.tykj.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.logging.log4j.util.PropertiesUtil;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -10,13 +11,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description TODO
  * @auther huran
  * @date
  **/
 public class LocationUtils {
-    // key
-    private static final String KEY = "WSHBZ-DJNCU-JRWVZ-BR7E2-ZTYA5-VEB3F";
+    static String KEY;
+
+    static {
+        PropertiesUtil propertiesUtil = new PropertiesUtil("coordinates.properties");
+        KEY = propertiesUtil.getStringProperty("KEY");
+    }
 
     /**
      * @Description: 通过经纬度获取位置
@@ -25,12 +29,14 @@ public class LocationUtils {
      * @Author: Alan
      * @Date: 2018/6/1 21:14
      */
-    public static Map<String, Object> getLocation(String lat, String lng,String key) {
+    public static Map<String, Object> getLocation(String lat, String lng, String key) {
 
         Map<String, Object> resultMap = new HashMap<String, Object>();
+        PropertiesUtil propertiesUtil = new PropertiesUtil("coordinates.properties");
 
         // 参数解释：lng：经度，lat：维度。KEY：腾讯地图key，get_poi：返回状态。1返回，0不返回
-        String urlString = "http://apis.map.qq.com/ws/geocoder/v1/?location=" + lat + "," + lng + "&key=" + key + "&get_poi=1";
+        String urlString = "http://apis.map.qq.com/ws/geocoder/v1/?location=" + lat + "," + lng + "&key=" + key +
+                "&get_poi=1";
         String result = "";
         try {
             URL url = new URL(urlString);
@@ -44,6 +50,7 @@ public class LocationUtils {
             while ((line = in.readLine()) != null) {
                 result += line + "\n";
             }
+            System.out.println(result);
             in.close();
         } catch (Exception e) {
             e.getMessage();
@@ -59,15 +66,17 @@ public class LocationUtils {
         resultMap.put("provinceCode", adInfo.get("adcode"));
         resultMap.put("city", adInfo.get("city"));
         resultMap.put("cityCode", adInfo.get("city_code"));
+        resultMap.put("district", adInfo.get("district"));
+
         return resultMap;
     }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
 
         // 测试
-        String lng = "111.546112";//经度
-        String lat = "24.378622";//维度
-        Map<String, Object> map = getLocation(lng, lat);
+        String lng = "34.946476";//经度
+        String lat = "113.170295";//维度
+        Map<String, Object> map = getLocation(lng, lat, KEY);
         System.out.println(map);
         System.out.println("国   籍：" + map.get("nation"));
         System.out.println("国家代码：" + map.get("nationCode"));
@@ -75,6 +84,8 @@ public class LocationUtils {
         System.out.println("省份代码：" + map.get("provinceCode"));
         System.out.println("城   市：" + map.get("city"));
         System.out.println("城市代码：" + map.get("cityCode"));
-    }*/
+        System.out.println("区：" + map.get("district"));
+
+    }
 
 }
