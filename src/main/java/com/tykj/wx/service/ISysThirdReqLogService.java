21257aaa78bef0a9fba5262963b36bc2031a22a9
BUCHU <710625937@qq.com>
2019-06-06 18:59:03 +0800

    发送消息到微信

---
+++ src/main/java/com/tykj/wx/service/ISysThirdReqLogService.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.service;
+
+import com.tykj.wx.entity.SysThirdReqLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* <p>
+    *  服务类
+    * </p>
+*
+* @author huran
+* @since 2019-06-06
+*/
+public interface ISysThirdReqLogService extends IService<SysThirdReqLog> {
+
+}
