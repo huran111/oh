d69f99ddd24b65d875b136feaa557d2eafaf805f
BUCHU <710625937@qq.com>
2019-06-06 10:04:15 +0800

    提交文件程序111111

--- src/main/java/com/tykj/common/SysConstant.java
+++ src/main/java/com/tykj/common/SysConstant.java
@@ -8,4 +8,19 @@ public class SysConstant {
     public static final String NOT_LOGIN = "not_login";
     public static final String PHONE_CHECK = "^1[3|4|5|7|8][0-9]\\d{4,8}$";
     public static final String DICTORY_TMP="tmpQrParam/";
+    /**
+     * 临时二维码标识
+     */
+    public  static final String TMP_QRPARAM="tmp";
+
+    /**
+     * 关闭
+     */
+    public  static final String SWITCH_0="0";
+
+    /**
+     * 开启
+     */
+    public  static final String SWITCH_1="1";
+
 }
