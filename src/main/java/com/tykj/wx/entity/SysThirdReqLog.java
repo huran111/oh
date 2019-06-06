807f013c8b0ba6643228ac6aa6f5185d430584de
BUCHU <710625937@qq.com>
2019-06-06 19:04:55 +0800

    发送消息到微信

--- src/main/java/com/tykj/wx/entity/SysThirdReqLog.java
+++ src/main/java/com/tykj/wx/entity/SysThirdReqLog.java
@@ -1,6 +1,7 @@
 package com.tykj.wx.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.tykj.core.BaseEntity;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
@@ -28,52 +29,64 @@ public class SysThirdReqLog extends BaseEntity {
     /**
      * 请求地址
      */
+    @TableField("req_url")
     private String reqUrl;
 
     /**
      * 请求类型
      */
+    @TableField("http_method")
     private String httpMethod;
 
     /**
      * 请求ip地址
      */
+    @TableField("req_ip")
     private String reqIp;
 
     /**
      * 类方法
      */
+    @TableField("class_method")
     private String classMethod;
 
     /**
      * 请求参数
      */
+    @TableField("req_args")
     private String reqArgs;
 
     /**
      * 请求时间
      */
+    @TableField("req_time")
     private String reqTime;
 
     /**
      * 方法内容
      */
+    @TableField("ret_args")
     private String retArgs;
 
     /**
      * 耗时
      */
+    @TableField("com_time")
     private String comTime;
 
     /**
      * 异常信息
      */
+    @TableField("err_msg")
     private String errMsg;
 
     /**
      * 标识
      */
+    @TableField("flag")
+
     private String flag;
+    @TableField("msg")
 
-
+    private String msg;
 }
