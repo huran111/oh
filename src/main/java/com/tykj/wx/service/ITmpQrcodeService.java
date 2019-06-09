f6df5d6f0054b3de10ef79530dfecc680582956f
huran111 <1345122890@qq.com>
2019-06-09 13:00:23 +0800

    提交小程序登陆 以及获取用户信息接口

--- src/main/java/com/tykj/wx/service/ITmpQrcodeService.java
+++ src/main/java/com/tykj/wx/service/ITmpQrcodeService.java
@@ -1,7 +1,11 @@
 package com.tykj.wx.service;
 
+import com.tykj.common.ApiResponse;
+import com.tykj.wx.dto.UserInfoDTO;
 import com.tykj.wx.entity.TmpQrcode;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.io.IOException;
 
 /**
 * <p>
@@ -13,4 +17,44 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface ITmpQrcodeService extends IService<TmpQrcode> {
 
+
+    /**
+     * 删除旧临时码保存新的临时码
+     * @param userInfoDTO
+     * @return
+     * @throws IOException
+     */
+    TmpQrcode   deleteOpenIdAndSaveTmpQrCode(UserInfoDTO userInfoDTO) throws IOException;
+
+    /**
+     * 编辑或者绑定信息
+     * @param userInfoDTO
+     * @return
+     */
+    ApiResponse editOrSave(UserInfoDTO userInfoDTO) throws Exception;
+
+    /**
+     * 判断用户是否绑定二维码信息
+     * @param qrParam
+     * @param openId
+     * @return
+     */
+    ApiResponse isbindingUserInfo(String qrParam, String openId);
+
+    /**
+     * 开启或者关闭通知
+     * @param qrParam
+     * @param openId
+     * @param isSwitch
+     * @return
+     */
+    ApiResponse onOrOffQr(String qrParam, String openId, String isSwitch);
+
+    /**
+     * 删除临时码
+     * @param qrParam
+     * @param openId
+     * @return
+     */
+    ApiResponse deleteTmpQr(String qrParam, String openId) throws Exception;
 }
