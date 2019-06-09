2c8f315f62f3c13bb4626cba606ec666c4860f4d
huran111 <1345122890@qq.com>
2019-06-09 13:02:01 +0800

    提交小程序登陆 以及获取用户信息接口

--- src/main/java/com/tykj/config/DatasourceConfig.java
+++ src/main/java/com/tykj/config/DatasourceConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
