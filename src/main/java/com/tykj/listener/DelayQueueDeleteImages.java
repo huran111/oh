6177cd4d7af6ee7bb11193a112632d448e3cdd07
BUCHU <710625937@qq.com>
2019-06-10 11:23:21 +0800

    发送消息到微信

--- src/main/java/com/tykj/listener/DelayQueueDeleteImages.java
+++ src/main/java/com/tykj/listener/DelayQueueDeleteImages.java
@@ -1,4 +1,4 @@
-package com.tykj.common;
+package com.tykj.listener;
 
 import lombok.Data;
 
@@ -6,24 +6,22 @@ import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Description TODO
  * @auther huran
  * @date
  **/
 @Data
+
 public class DelayQueueDeleteImages implements Delayed {
     private String id;
     private long time;
-    public DelayQueueDeleteImages(String id,long delayTime){
+    public DelayQueueDeleteImages(String id, long delayTime){
         this.id=id;
         this.time= TimeUnit.NANOSECONDS.convert(delayTime, TimeUnit.MILLISECONDS) + System.nanoTime();  ;
     }
-    //// 延迟任务是否到时就是按照这个方法判断如果返回的是负数则说明到期否则还没到期
     @Override
     public long getDelay(TimeUnit unit) {
         return unit.convert(this.time - System.nanoTime(), TimeUnit.NANOSECONDS);
     }
-    //// 自定义实现比较方法返回 1 0 -1三个参数
     @Override
     public int compareTo(Delayed delayed) {
         DelayQueueDeleteImages msg = (DelayQueueDeleteImages) delayed;
