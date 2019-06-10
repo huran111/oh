24b064a919595355b97e826eae97fd9bb56ad270
BUCHU <710625937@qq.com>
2019-06-10 20:14:14 +0800

    删除临时二维码

--- src/main/java/com/tykj/utils/DateUtils.java
+++ src/main/java/com/tykj/utils/DateUtils.java
@@ -1,5 +1,6 @@
 package com.tykj.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,5 +17,32 @@ public class DateUtils {
         nowTime.add(Calendar.MINUTE, 6);
         System.out.println(sdf.format(nowTime.getTime()));
         return sdf.format(nowTime.getTime());
+    }
+    /**
+     * 时间戳
+     * @return
+     */
+    public static String CreateDate(){
+        String dataStr=null;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
+        dataStr = sdf.format(new Date());
+        return dataStr;
+    }
+    /**
+     * 失效时间 当前时间+10min
+     * @return
+     */
+    public static String expriredDate(String dataStr){
+        String aDataStr=null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
+            Date date = sdf.parse(dataStr);
+            date.setTime(date.getTime()+5*60*1000);
+            aDataStr=sdf.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return aDataStr;
     }
 }
