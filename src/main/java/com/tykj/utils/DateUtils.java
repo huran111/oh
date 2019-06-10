2b818fe3670dffef3cb82f568312c9959cabdf1a
BUCHU <710625937@qq.com>
2019-06-10 20:18:53 +0800

    删除临时二维码

--- src/main/java/com/tykj/utils/DateUtils.java
+++ src/main/java/com/tykj/utils/DateUtils.java
@@ -2,6 +2,7 @@ package com.tykj.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,39 +11,37 @@ import java.util.Date;
  * @date
  **/
 public class DateUtils {
-   static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    public static String addSeconds(){
+    public static String addSeconds() {
         Calendar nowTime = Calendar.getInstance();
         nowTime.add(Calendar.MINUTE, 6);
         System.out.println(sdf.format(nowTime.getTime()));
         return sdf.format(nowTime.getTime());
     }
+
     /**
      * 时间戳
+     *
      * @return
      */
-    public static String CreateDate(){
-        String dataStr=null;
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-        dataStr = sdf.format(new Date());
-        return dataStr;
+    public static Long CreateDate() {
+        return System.currentTimeMillis();
     }
+
     /**
      * 失效时间 当前时间+10min
+     *
      * @return
      */
-    public static String expriredDate(String dataStr){
-        String aDataStr=null;
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmss");
-            Date date = sdf.parse(dataStr);
-            date.setTime(date.getTime()+5*60*1000);
-            aDataStr=sdf.format(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return aDataStr;
+    public static Long expriredDate(Long time) {
+        return time += 5 * 1000 * 60;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("获取当前系统时间为："
+                +new SimpleDateFormat("yyyy年-MM月dd日-HH时mm分ss秒").format(CreateDate()));//转换成标准年月日的形式
+        System.out.println("获取当前系统时间为："
+                +new SimpleDateFormat("yyyy年-MM月dd日-HH时mm分ss秒").format(expriredDate(CreateDate())));//转换成标准年月日的形式
     }
 }
