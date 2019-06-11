bca0c262fa8aef0b1795adb6bfbe1dcd78d2f6d5
BUCHU <710625937@qq.com>
2019-06-11 09:53:32 +0800

    删除临时二维码

--- src/main/java/com/tykj/interceptor/LoginInterceptor.java
+++ src/main/java/com/tykj/interceptor/LoginInterceptor.java
@@ -27,11 +27,7 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-      /*  System.out.println(">>>>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
-        System.out.println(request.getMethod());
-        System.out.println(request.getRequestURI());
-        System.out.println(request.getRequestURL());
-
+      /*
         String sessionId = request.getParameter(SysConstant.SESSION_KEY);
         IAccessTokenCache accessTokenCache = ApiConfigKit.getAccessTokenCache();
         String sessionJson = accessTokenCache.get("wxa:session:" + sessionId);
@@ -45,7 +41,7 @@ public class LoginInterceptor implements HandlerInterceptor {
             // response.sendRedirect(request.getContextPath()+"/");
             return false;
         }*/
-        log.info("======================>>>拦截开始..............");
+        log.info("======================>>>拦截开始..............[{}]",handler.toString());
         return true;
     }
 
