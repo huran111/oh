901f1fce883bf679503cb504210ec493e9c02b89
huran111 <1345122890@qq.com>
2019-06-11 22:18:09 +0800

    提交小程序登陆 以及获取用户信息接口

--- src/main/java/com/tykj/msg/SendTemplateMsg.java
+++ src/main/java/com/tykj/msg/SendTemplateMsg.java
@@ -3,6 +3,8 @@ package com.tykj.msg;
 import com.jfinal.weixin.sdk.api.ApiResult;
 import com.jfinal.weixin.sdk.api.TemplateData;
 import com.jfinal.weixin.sdk.api.TemplateMsgApi;
+import com.jfinal.wxaapp.api.WxaTemplate;
+import com.jfinal.wxaapp.api.WxaTemplateApi;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
@@ -21,39 +23,42 @@ public abstract class SendTemplateMsg {
     public void sendTemplateMsg(String openId, String plate, String address, String token) {
         log.info("发送模板消息:openId:[{}] plate:[{}] address:[{}],token:[{}]"
                 , openId, plate, address, token);
-        // 模板消息，发送测试：pass
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String message = "感谢您使用【欧海挪车】通知车主挪车，欧海挪车为广大车主提供更安全更便捷的智慧挪车服务";
-        String remark = "点击申请我的挪车吗，畅享智慧车生活";
-        if (null == address) {
-            ApiResult result = TemplateMsgApi.send(TemplateData.New()
-                    // 消息接收者
-                    .setTouser(openId)
-                    // 模板id
-                    .setTemplate_id("QG4_bKOjuNdkWOIVqk8jRUykN0o9Y1lsP0YKH9KeGmw").setUrl("https://api.weixin.qq" +
-                            "" + ".com/cgi-bin/message/wxopen/template/send?access_token=" + token + "")
+        try {
+            // 模板消息，发送测试：pass
+            DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String message = "感谢您使用【欧海挪车】通知车主挪车，欧海挪车为广大车主提供更安全更便捷的智慧挪车服务";
+            String remark = "点击申请我的挪车吗，畅享智慧车生活";
+            if (null == address) {
+                WxaTemplateApi wxaTemplateApi=new WxaTemplateApi();
+                WxaTemplate wxaTemplate=new WxaTemplate();
+                wxaTemplate.setTemplate_id("QG4_bKOjuNdkWOIVqk8jRUykN0o9Y1lsP0YKH9KeGmw");
+                wxaTemplate.setEmphasis_keyword("keyword1.DATA")
+                        .setTouser(openId)
+                        .add("keyword1",plate,"000")
+                        .add("keyword2",df.format(LocalDateTime.now()), "#333")
 
-                    // 模板参数
-                    .add("keyword1", plate, "#000").add("keyword2", df.format(LocalDateTime.now()), "#333").add
-                            ("keyword4", message, "#333").add("keyword5", remark).add("emphasis_keyword",
-                            "keyword1.DATA").build());
-            log.info("是否成功:[{}]",result.isSucceed());
-            System.out.println(result.toString());
-        } else {
-            ApiResult result = TemplateMsgApi.send(TemplateData.New()
-                    // 消息接收者
-                    .setTouser(openId)
-                    // 模板id
-                    .setTemplate_id("QG4_bKOjuNdkWOIVqk8jRYj0Z9XHJU84Ij5rxWp3_Qs").setUrl("https://api.weixin.qq" + "" +
-                            ".com/cgi-bin/message/wxopen/template/send?access_token=" + token + "")
-
-                    // 模板参数
-                    .add("keyword1", plate, "#000").add("keyword2", df.format(LocalDateTime.now()), "#333").add
-                            ("keyword3", address, "#333").add("keyword4", message, "#333").add("keyword5", remark).add
-                            ("emphasis_keyword", "keyword1.DATA").build());
-            log.info("是否成功:[{}]",result.isSucceed());
-            System.out.println(result.toString());
-
+                        .add("keyword4", message, "#333")
+                        .add("keyword5", remark);
+                ApiResult apiResult= wxaTemplateApi.send(wxaTemplate);
+                System.out.println(apiResult.isSucceed());
+            } else {
+                WxaTemplateApi wxaTemplateApi=new WxaTemplateApi();
+                WxaTemplate wxaTemplate=new WxaTemplate();
+                wxaTemplate.setTemplate_id("QG4_bKOjuNdkWOIVqk8jRUykN0o9Y1lsP0YKH9KeGmw");
+                wxaTemplate.setEmphasis_keyword("keyword1.DATA")
+                        .setTouser(openId)
+                        .add("keyword1",plate,"000")
+                        .add("keyword2",df.format(LocalDateTime.now()), "#333")
+                        .add("keyword3", address, "#333")
+                        .add("keyword4", message, "#333")
+                        .add("keyword5", remark);
+                ApiResult apiResult= wxaTemplateApi.send(wxaTemplate);
+                System.out.println(apiResult.isSucceed());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+
     }
 }
