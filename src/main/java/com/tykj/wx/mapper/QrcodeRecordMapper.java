1d8671475bde5abdb4145822961f1e448c2e2062
huran111 <1345122890@qq.com>
2019-06-12 21:23:30 +0800

    提交小程序登陆 以及获取用户信息接口

---
+++ src/main/java/com/tykj/wx/mapper/QrcodeRecordMapper.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.mapper;
+
+import com.tykj.wx.entity.QrcodeRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* <p>
+    *  Mapper 接口
+    * </p>
+*
+* @author huran
+* @since 2019-06-12
+*/
+public interface QrcodeRecordMapper extends BaseMapper<QrcodeRecord> {
+
+}
---
+++ src/main/java/com/tykj/wx/mapper/TmpqrcodeRecordMapper.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.mapper;
+
+import com.tykj.wx.entity.TmpqrcodeRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* <p>
+    *  Mapper 接口
+    * </p>
+*
+* @author huran
+* @since 2019-06-12
+*/
+public interface TmpqrcodeRecordMapper extends BaseMapper<TmpqrcodeRecord> {
+
+}
