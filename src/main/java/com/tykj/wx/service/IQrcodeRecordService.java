e1e5af3f6647e0d1ad508b5bd6fd6095f47813e9
huran111 <1345122890@qq.com>
2019-06-12 21:22:44 +0800

    提交小程序登陆 以及获取用户信息接口

---
+++ src/main/java/com/tykj/wx/service/IQrcodeRecordService.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.service;
+
+import com.tykj.wx.entity.QrcodeRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* <p>
+    *  服务类
+    * </p>
+*
+* @author huran
+* @since 2019-06-12
+*/
+public interface IQrcodeRecordService extends IService<QrcodeRecord> {
+
+}
---
+++ src/main/java/com/tykj/wx/service/ITmpqrcodeRecordService.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.service;
+
+import com.tykj.wx.entity.TmpqrcodeRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* <p>
+    *  服务类
+    * </p>
+*
+* @author huran
+* @since 2019-06-12
+*/
+public interface ITmpqrcodeRecordService extends IService<TmpqrcodeRecord> {
+
+}
