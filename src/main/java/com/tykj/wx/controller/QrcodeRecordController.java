cb08cdf986a3e1ee29c3551b77329b85023c1e84
huran111 <1345122890@qq.com>
2019-06-12 21:56:14 +0800

    提交小程序登陆 以及获取用户信息接口

--- src/main/java/com/tykj/wx/controller/QrcodeRecordController.java
+++ src/main/java/com/tykj/wx/controller/QrcodeRecordController.java
@@ -1,29 +1,58 @@
 package com.tykj.wx.controller;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tykj.common.ApiCode;
+import com.tykj.common.ApiResponse;
+import com.tykj.common.SysConstant;
 import com.tykj.wx.entity.QrcodeRecord;
+import com.tykj.wx.entity.TmpqrcodeRecord;
 import com.tykj.wx.service.IQrcodeRecordService;
 
+import com.tykj.wx.service.ITmpqrcodeRecordService;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.tykj.core.web.BaseController;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
-* <p>
-    *  前端控制器
-    * </p>
-*
-* @author huran
-* @since 2019-06-12
-*/
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author huran
+ * @since 2019-06-12
+ */
 @RestController
-@RequestMapping("rest//wx/qrcode-record")
-    public class QrcodeRecordController extends BaseController<IQrcodeRecordService,QrcodeRecord> {
+@RequestMapping("rest/wx/qrcoderecord")
+public class QrcodeRecordController extends BaseController<IQrcodeRecordService, QrcodeRecord> {
+    @Autowired
+    ITmpqrcodeRecordService tmpqrcodeRecordService;
+    @Autowired
+    IQrcodeRecordService qrcodeRecordService;
 
+    @GetMapping(value = "viewRecord")
+    public ApiResponse viewRecord(@RequestParam(value = "qrParam") String qrParam) {
+        if (qrParam.contains(SysConstant.TMP_QRPARAM)) {
+            List<TmpqrcodeRecord> qrcodeRecords = tmpqrcodeRecordService.list(new QueryWrapper<TmpqrcodeRecord>()
+                    .lambda().eq(TmpqrcodeRecord::getQrParam, qrParam));
+            if (CollectionUtils.isNotEmpty(qrcodeRecords)) {
+              return   new ApiResponse<>(ApiCode.REQUEST_SUCCESS, qrcodeRecords);
+            }else {
+                return   new ApiResponse<>(ApiCode.REQUEST_SUCCESS, new ArrayList<>());
+            }
+        } else {
+            List<QrcodeRecord> qrcodeRecords = qrcodeRecordService.list(new QueryWrapper<QrcodeRecord>()
+                    .lambda().eq(QrcodeRecord::getQrParam, qrParam));
+            if (CollectionUtils.isNotEmpty(qrcodeRecords)) {
+                return   new ApiResponse<>(ApiCode.REQUEST_SUCCESS, qrcodeRecords);
+            }else {
+                return   new ApiResponse<>(ApiCode.REQUEST_SUCCESS, new ArrayList<>());
+            }
+        }
+    }
 }
