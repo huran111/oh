07897ba8a2e4571a9f405124cd87d264341b9650
huran111 <134522890@qq.com>
2019-06-15 15:57:42 +0800

    文件数量为2000

---
+++ src/main/java/com/tykj/job/ImagesTask.java
@@ -0,0 +1,29 @@
+package com.tykj.job;
+
+import com.jfinal.weixin.sdk.utils.IOUtils;
+import com.jfinal.wxaapp.api.WxaQrcodeApi;
+import com.tykj.utils.UUIDUtils;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ImagesTask implements Callable<Long> {
+    private WxaQrcodeApi wxaQrcodeApi;
+    private String day;
+    private AtomicLong atomicLong;
+
+    public ImagesTask(WxaQrcodeApi wxaQrcodeApi1, String day, AtomicLong atomicLong) {
+        this.wxaQrcodeApi = wxaQrcodeApi1;
+        this.day = day;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        String qrParamId = UUIDUtils.getUUID();
+        InputStream inputStream = wxaQrcodeApi.getUnLimit(qrParamId, "pages/home/home");
+        IOUtils.toFile(inputStream, new File("/home/images/qrParam/" + day + "/" + qrParamId + ".png"));
+        return atomicLong.incrementAndGet();
+    }
+}
