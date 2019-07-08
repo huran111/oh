3ae2545db4de36865e3913c339268722850fd124
BUCHU <710625937@qq.com>
2019-07-08 13:10:50 +0800

    删除临时二维码

--- src/main/java/com/tykj/aliyun/properties/AliYunProperties.java
+++ src/main/java/com/tykj/aliyun/properties/AliYunProperties.java
@@ -1,5 +1,4 @@
 package com.tykj.aliyun.properties;
-
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -11,17 +10,18 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "aliyun")
 @Component
 public class AliYunProperties{
-	private String regionId;
-	private String accessKeyId;
-	private String secret;
-	private String domain;
-	private String version;
-	private String smsDomain;
-	private String plDomain;
-	private String signName;
-	private String templateCode;
-	private String poolKey;
-	private String bindAxn;
-	private String sendSms;
-	private String addressKey;
+    private String regionId;
+    private String accessKeyId;
+    private String secret;
+    private String domain;
+    private String version;
+    private String smsDomain;
+    private String plDomain;
+    private String signName;
+    private String templateCode;
+    private String poolKey;
+    private String bindAxn;
+    private String sendSms;
+    private String addressKey;
+    private String templateReserveCode;
 }
