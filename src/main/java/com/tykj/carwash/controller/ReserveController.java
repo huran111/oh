1031714ea339901ad253aae7270a2cbddba711cf
ZJZ <546935036@qq.com>
2019-07-09 16:09:51 +0800

    完善预约洗车模块

--- src/main/java/com/tykj/carwash/controller/ReserveController.java
+++ src/main/java/com/tykj/carwash/controller/ReserveController.java
@@ -1,8 +1,11 @@
 package com.tykj.carwash.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.google.common.collect.Lists;
 import com.tykj.aliyun.controller.SendSms;
-import com.tykj.carwash.dto.ReserveDTO;
+import com.tykj.carwash.dto.ParameterDTO;
+import com.tykj.carwash.vo.ReserveVO;
 import com.tykj.carwash.entity.Reserve;
 import com.tykj.carwash.entity.Shops;
 import com.tykj.carwash.service.ReserveService;
@@ -10,12 +13,15 @@ import com.tykj.carwash.service.ShopsService;
 import com.tykj.common.ApiCode;
 import com.tykj.common.ApiResponse;
 import com.tykj.utils.AESUtils;
+import com.tykj.utils.LngLatUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @program: tykj-system
@@ -35,20 +41,35 @@ public class ReserveController {
 	@Autowired
 	private SendSms sendSms;
 
+	/**
+	 * 保存预约洗车记录
+	 *
+	 * @param parameterDTO 参数
+	 * @return
+	 * @throws Exception
+	 */
 	@PostMapping("/save")
-	public ApiResponse save(@RequestBody ReserveDTO reserveDTO) throws Exception {
-		Shops shop = shopsService.getById(reserveDTO.getStoreId());
+	public ApiResponse save(@RequestBody ParameterDTO parameterDTO) throws Exception {
+
+		QueryWrapper<Reserve> condition = new QueryWrapper<>();
+		condition.lambda().eq(Reserve::getOpenId, parameterDTO.getOpenId());
+		condition.lambda().apply("date(create_time) = curdate()");
+		int count = reserveService.count(condition);
+		if (count > 3) {
+			return new ApiResponse(ApiCode.OPERATOR_FAIL, "当天预约次数不得超过三次");
+		}
+		Shops shop = shopsService.getById(parameterDTO.getStoreId());
 		if (null == shop) {
 			return new ApiResponse(ApiCode.OPERATOR_FAIL);
 		}
-		String sessionKey = stringRedisTemplate.opsForValue().get("sessionKey:" + reserveDTO.getOpenId());
-		String info = AESUtils.decrypt(reserveDTO.getEncryptedData(), sessionKey, reserveDTO.getIv(), "UTF-8");
+		String sessionKey = stringRedisTemplate.opsForValue().get("sessionKey:" + parameterDTO.getOpenId());
+		String info = AESUtils.decrypt(parameterDTO.getEncryptedData(), sessionKey, parameterDTO.getIv(), "UTF-8");
 		String userPhone = (String) JSONObject.parseObject(info).get("phoneNumber");
 		Reserve reserve = new Reserve();
-		reserve.setCreateTime(new Date()).setReserveTime(reserveDTO.getReserveTime()).
-				setUserPhone(userPhone).setOpenId(reserveDTO.getOpenId()).setStoreId(shop.getId()).setStorePhone(shop.getPhone());
+		reserve.setCreateTime(new Date()).setReserveTime(parameterDTO.getReserveTime()).
+				setUserPhone(userPhone).setOpenId(parameterDTO.getOpenId()).setStoreId(shop.getId()).setStorePhone(shop.getPhone());
 		reserveService.save(reserve);
-		ApiResponse apiResponse = this.sendSms.sendReserveMsg(userPhone, shop.getPhone(), reserveDTO.getReserveTime(), shop.getStoreName());
+		ApiResponse apiResponse = this.sendSms.sendReserveMsg(userPhone, shop.getPhone(), parameterDTO.getReserveTime(), shop.getStoreName());
 		if (apiResponse.getStatus() == 200) {
 			return new ApiResponse(ApiCode.OPERATOR_SUCCESS);
 		}
@@ -56,8 +77,55 @@ public class ReserveController {
 
 	}
 
+	/**
+	 * 获取用户续约记录
+	 *
+	 * @param openId 用户openid
+	 * @param lng    经度
+	 * @param lat    纬度
+	 * @return
+	 */
 	@GetMapping("/list")
-	public ApiResponse list() {
-		return null;
+	public ApiResponse list(
+			@RequestParam("openId") String openId, @RequestParam(value = "longitude") String lng,
+			@RequestParam(value = "latitude") String lat) {
+		QueryWrapper<Reserve> condition = new QueryWrapper<>();
+		condition.lambda().eq(Reserve::getOpenId, openId);
+		condition.lambda().eq(Reserve::getIsReserve, 0);
+		condition.lambda().apply("create_time >= now()-interval 3 day");
+		condition.lambda().orderByDesc(true, Reserve::getCreateTime);
+		List<Reserve> reserves = reserveService.list(condition);
+		if (CollectionUtils.isEmpty(reserves)) {
+			return new ApiResponse(ApiCode.OPERATOR_FAIL, reserves);
+		}
+		Shops shop = shopsService.getById(reserves.get(0).getStoreId());
+		List<ReserveVO> reserveArrayList = Lists.newArrayList();
+		for (Reserve reserve : reserves) {
+			ReserveVO reserveVO = new ReserveVO();
+			double distance = LngLatUtils.getDistance(Double.parseDouble(lng), Double.parseDouble(lat), shop.getLongitude(), shop.getLatitude());
+			reserveVO.setAddress(shop.getAddress()).setDistance(distance).setGrade(shop.getGrade()).setLatitude(shop.getLatitude()).
+					setLongitude(shop.getLongitude()).setImage(shop.getImage()).setPhone(shop.getPhone()).setRefPrice(shop.getRefPrice()).setSales(shop.getSales()).
+					setStoreId(shop.getId()).setReserveTime(reserve.getReserveTime()).setStoreName(shop.getStoreName());
+			reserveArrayList.add(reserveVO);
+		}
+		return new ApiResponse(ApiCode.SEND_SUCCESS, reserveArrayList);
+	}
+
+	/**
+	 * 取消预约
+	 *
+	 * @param id 预约记录的id
+	 * @return
+	 */
+	@GetMapping("/cancelReserve")
+	public ApiResponse cancelReserve(@RequestParam("id") String id) {
+		Reserve reserve = reserveService.getById(id);
+		if (null == reserve) {
+			return new ApiResponse(ApiCode.OPERATOR_FAIL, null);
+		}
+		reserve.setIsReserve(1);
+		reserveService.updateById(reserve);
+		return new ApiResponse(ApiCode.OPERATOR_SUCCESS);
+
 	}
 }
