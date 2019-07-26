6487f885e5aec27542abd588b7b0cb0df3c0f110
BUCHU <710625937@qq.com>
2019-07-26 19:08:22 +0800

    记录生成的二维码

---
+++ src/main/java/com/tykj/wx/mapper/JobParamRecordMapper.java
@@ -0,0 +1,16 @@
+package com.tykj.wx.mapper;
+
+import com.tykj.wx.entity.JobParamRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* <p>
+    * job生成的线下二维码记录 Mapper 接口
+    * </p>
+*
+* @author huran
+* @since 2019-07-26
+*/
+public interface JobParamRecordMapper extends BaseMapper<JobParamRecord> {
+
+}
